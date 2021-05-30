4636696116d06afc31cd6e3153dfcfd4b12e2538
kruemelkeksfan <37933687+kruemelkeksfan@users.noreply.github.com>
2021-05-30 21:23:39 +0200

    Fixed only one EnergyConsumer getting supplied per Update.

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -286,7 +286,6 @@ public class InventoryController : MonoBehaviour, IListener
 			}
 
 			int consumerCounter = 0;
-			consumerIndex = energyConsumers.Count > 0 ? (consumerIndex + 1) % energyConsumers.Count : 0;
 			while(energy > 0.0f && consumerCounter < energyConsumers.Count)
 			{
 				energy = energyConsumers[consumerIndex].Charge(energy);
@@ -298,6 +297,7 @@ public class InventoryController : MonoBehaviour, IListener
 					}
 					energy = energyConsumers[consumerIndex].Charge(energy);
 				}
+				consumerIndex = (consumerIndex + 1) % energyConsumers.Count;
 
 				++consumerCounter;
 			}
