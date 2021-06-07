e4097667727fa2af5d034bcf2045ae67d563cdf3
kruemelkeksfan <37933687+kruemelkeksfan@users.noreply.github.com>
2021-06-07 22:56:32 +0200

    Reworked Camera Controller to no longer be a Child of the Player Spacecraft, implemented new Default Camera Mode which keeps the Camera stationary against the Background and added Camera Mode Switch Button.

--- Assets/Scripts/Controls/CameraController.cs
+++ Assets/Scripts/Controls/CameraController.cs
@@ -3,28 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CameraController : MonoBehaviour
+public class CameraController : MonoBehaviour, IListener
 {
 	[SerializeField] private float movementSpeed = 1.0f;
 	[SerializeField] private float rotationSpeed = 1.0f;
 	[SerializeField] private float zoomSpeed = 1.0f;
 	[SerializeField] private float maxZHeight = -0.04f;
 	[SerializeField] private float minZHeight = -2000.0f;
-	[SerializeField] private Transform spacecraftTransform = null;
 	private new Transform transform = null;
+	private Transform spacecraftTransform = null;
 	private Vector3 startPosition = Vector3.zero;
-	private Quaternion startRotation = Quaternion.identity;
+	private Vector3 startRotation = Vector3.zero;
+	private Vector3 localPosition;
+	private Vector3 localRotation;
+	private bool fixedCamera = true;
 
 	private void Start()
 	{
 		transform = gameObject.GetComponent<Transform>();
 
-		startPosition = transform.localPosition;
-		startRotation = transform.localRotation;
+		startPosition = transform.position;
+		startRotation = transform.rotation.eulerAngles;
+
+		localPosition = startPosition;
+		localRotation = startRotation;
+
+		SpacecraftManager.GetInstance().AddSpacecraftChangeListener(this);
+		Notify();
 	}
 
 	private void Update()
 	{
+		if(Input.GetButtonUp("Camera Mode"))
+		{
+			fixedCamera = !fixedCamera;
+		}
+
+		Quaternion rotation = Quaternion.Euler(localRotation);
+		if(!fixedCamera)
+		{
+			rotation = spacecraftTransform.rotation * rotation;
+		}
+		transform.position = spacecraftTransform.position + (rotation * localPosition);
+		transform.rotation = rotation;
+
+		// TODO: Collider for Camera
 		if(Input.GetButton("Rotate Camera"))
 		{
 			Cursor.visible = false;
@@ -48,10 +71,9 @@ public class CameraController : MonoBehaviour
 				direction += transform.right;
 			}
 
-			float directionMultiplier = Vector3.Dot(transform.rotation * Vector3.forward, Vector3.forward) * 1.2f - 0.2f;                                               // Turn Camera slower when looking at a flatter Angle
-			transform.RotateAround(spacecraftTransform.position, transform.right, -Input.GetAxis("Mouse Y") * rotationSpeed * directionMultiplier);
-			transform.RotateAround(spacecraftTransform.position, transform.up, Input.GetAxis("Mouse X") * rotationSpeed * directionMultiplier);
-			transform.position += direction * movementSpeed * -transform.position.z;
+			float directionMultiplier = (Vector3.Dot(transform.forward, Vector3.forward) * 1.2f - 0.2f) * rotationSpeed;                                               // Turn Camera slower when looking at a flatter Angle
+			localPosition += direction * movementSpeed * -localPosition.z;
+			localRotation += new Vector3(-Input.GetAxis("Mouse Y") * directionMultiplier, Input.GetAxis("Mouse X") * directionMultiplier, 0.0f);
 		}
 		else
 		{
@@ -61,21 +83,26 @@ public class CameraController : MonoBehaviour
 
 		if(Input.GetButtonUp("Reset Camera"))
 		{
-			transform.localPosition = startPosition;
-			transform.localRotation = startRotation;
+			localPosition = startPosition;
+			localRotation = startRotation;
 		}
 
-		if(!EventSystem.current.IsPointerOverGameObject())
+		if(!EventSystem.current.IsPointerOverGameObject() && !Mathf.Approximately(Input.GetAxis("Mouse ScrollWheel"), 0.0f))
 		{
-			transform.position += transform.forward * Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * -transform.position.z;
-			if(transform.position.z > maxZHeight)
+			localPosition += transform.forward * Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * -localPosition.z;
+			if(localPosition.z > maxZHeight)
 			{
-				transform.position = new Vector3(transform.position.x, transform.position.y, maxZHeight);
+				localPosition = new Vector3(localPosition.x, localPosition.y, maxZHeight);
 			}
-			else if(transform.position.z < minZHeight)
+			else if(localPosition.z < minZHeight)
 			{
-				transform.position = new Vector3(transform.position.x, transform.position.y, minZHeight);
+				localPosition = new Vector3(localPosition.x, localPosition.y, minZHeight);
 			}
 		}
+	}
+
+	public void Notify()
+	{
+		spacecraftTransform = SpacecraftManager.GetInstance().GetLocalPlayerMainSpacecraft().GetTransform();
 	}
 }
